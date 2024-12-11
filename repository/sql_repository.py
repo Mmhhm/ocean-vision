200a00acec97e575da02d87137e16e3082be468d
Yoni <yonie3921@gmail.com>
2024-12-11 17:56:43 +0200

    Initial new_dev branch

--- repository/sql_repository.py
+++ repository/sql_repository.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from main_app.db_config.postgres_conn import db_session
+from main_app.db_service.sql.sqlalchemy_models import ShipModel, SubmarineModel
+
 def insert_into_ship_table(data):
     """
     Inserts data into the SHIP table.
@@ -5,8 +10,28 @@ def insert_into_ship_table(data):
     Args:
         data (dict): The data to insert.
     """
-    print(f"Inserting into SHIP table: {data}")
-    # Placeholder for actual database insertion logic
+    ship_data = data.get("data", {})
+
+    new_ship = ShipModel(
+        type=ship_data["type"],
+        mmsi=ship_data.get("submarine_id"),  # Assuming 'submarine_id' for now
+        timestamp=datetime.fromisoformat(data.get("data", {}).get("timestamp")),
+        lat=ship_data['detected_object']['latitude'],
+        lon=ship_data['detected_object']['longitude'],
+        object_size=ship_data['detected_object']["object_size"],
+        heat_temperature_celsius=ship_data['detected_object']["heat_temperature_celsius"],
+        hostility=ship_data['detected_object']["hostility"],
+        danger=ship_data['detected_object']["danger"],
+        speed_knots=ship_data['detected_object']["speed_knots"],
+        bearing_degrees=ship_data['detected_object']["bearing_degrees"],
+        distance_km=ship_data['detected_object']["distance_km"],
+        status=ship_data['detected_object']["status"]
+    )
+
+    db_session.add(new_ship)
+    db_session.commit()
+    print(f"Inserted into SHIP table: {new_ship}")
+    return new_ship
 
 def insert_into_submarine_table(data):
     """
@@ -15,5 +40,28 @@ def insert_into_submarine_table(data):
     Args:
         data (dict): The data to insert.
     """
+    submarine_data = data.get("data", {})
+
+    new_submarine = SubmarineModel(
+        type=submarine_data["type"],
+        timestamp=datetime.fromisoformat(data.get("data", {}).get("timestamp")),
+        submarine_id=submarine_data['detected_object']["submarine_id"],
+        object_size=submarine_data['detected_object']["object_size"],
+        object_type= submarine_data['detected_object']["object_type"],
+        object_shape=submarine_data['detected_object']["object_shape"],
+        object_material=submarine_data['detected_object']["object_material"],
+        distance_km=submarine_data['detected_object']["distance_km"],
+        depth_meters=submarine_data['detected_object']["depth_meters"],
+        danger=submarine_data['detected_object']["danger"],
+        bearing_degrees=submarine_data['detected_object']["bearing_degrees"],
+        speed_knots=submarine_data['detected_object']["speed_knots"],
+        heat_temperature_celsius=submarine_data['detected_object']["heat_temperature_celsius"],
+        hostility=submarine_data['detected_object']["hostility"],
+        status=submarine_data['detected_object']["status"],
+        lat=submarine_data['detected_object']["lat"],
+        lon=submarine_data['detected_object']["lon"],
+    )
+    db_session.add(new_submarine)
+    db_session.commit()
     print(f"Inserting into SUBMARINE table: {data}")
-    # Placeholder for actual database insertion logic
+    return new_submarine
